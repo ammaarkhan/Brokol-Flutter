6bf445ced0d95a5e99323b49b881bda265b2e779
ammaarkhan <ammaarkhan03@gmail.com>
2023-04-18 18:19:30 -0700

    implement date picker for expiry

--- brokol_flutter/lib/pages/items_screen.dart
+++ brokol_flutter/lib/pages/items_screen.dart
@@ -1,6 +1,8 @@
+import 'package:brokol_flutter/services/category_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:intl/intl.dart';
 
 class ItemScreen extends StatefulWidget {
   const ItemScreen({super.key});
@@ -10,12 +12,49 @@ class ItemScreen extends StatefulWidget {
 }
 
 class _ItemScreen extends State<ItemScreen> {
-  var itemNameController = TextEditingController();
-  var itemExpiryController = TextEditingController();
+  var _itemNameController = TextEditingController();
+  var _itemExpiryController = TextEditingController();
 
   var _selectedValue;
 
-  var _categories;
+  List<DropdownMenuItem> _categories = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCategories();
+  }
+
+  _loadCategories() async {
+    var _categoriesService = CategoryService();
+    var categories = await _categoriesService.readCategories();
+    categories.forEach((category) {
+      setState(() {
+        _categories.add(DropdownMenuItem(
+          child: Text(category['name']),
+          value: category['name'],
+        ));
+      });
+    });
+  }
+
+  DateTime _dateTime = DateTime.now();
+
+  _selectedToDoDate(BuildContext context) async {
+    var _pickedDate = await showDatePicker(
+        context: context,
+        initialDate: _dateTime,
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2100));
+
+    if (_pickedDate != null) {
+      setState(() {
+        _dateTime = _pickedDate;
+        _itemExpiryController.text =
+            DateFormat('dd-MM-yyyy').format(_pickedDate);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +66,7 @@ class _ItemScreen extends State<ItemScreen> {
         padding: EdgeInsets.all(16.0),
         child: Column(children: <Widget>[
           TextField(
-            controller: itemNameController,
+            controller: _itemNameController,
             decoration: InputDecoration(
               labelText: 'Name',
               hintText: 'Add New Item Name',
@@ -39,12 +78,14 @@ class _ItemScreen extends State<ItemScreen> {
           //   hintText: 'Add New Description Name',),
           // ),
           TextField(
-            controller: itemExpiryController,
+            controller: _itemExpiryController,
             decoration: InputDecoration(
                 labelText: 'Expiry',
                 hintText: 'Pick an Expiry Date',
                 prefixIcon: InkWell(
-                  onTap: () {},
+                  onTap: () {
+                    _selectedToDoDate(context);
+                  },
                   child: Icon(Icons.calendar_today),
                 )),
           ),
